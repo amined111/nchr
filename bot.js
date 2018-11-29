c12b42f4f74ecb0ec642a47ea2fb6833da9bf954
amined111 <43893689+amined111@users.noreply.github.com>
2018-11-29 23:37:01 +0100

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,27 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+let timer;
+
+let d5loa = [];
+
+console.log("BOT ONLINE");
+
+client.on("guildMemberAdd", member => {
+
+       if(member.guild.id === '510402549619228682') {
+           d5loa.push(member.user.id)
+      }
+
+    if(d5loa.includes(member.user.id)) return;
+
+
+    timer = Math.floor(Math.random() * (10000 - 5000 + 1)) + 5000
+  let words = [`يا هلا بالقمر الي جانا ونور سمانا .. بدون وجودك أنسى من هو أنا , :sparkles:`,`مثل مابقيـــنا , بوجودك إلتَّميــــنا , وتشرفـــنا بيكك ياقمــــــــرنا ,, :sparkles::kiss:`,`نورنا`]
+  setTimeout(() =>{
+  member.createDM().then(function (channel) {
+  return channel.send(`${words[Math.floor(Math.random() * words.length)]}`) 
+}).catch(console.error)
+}, timer)
+})
+    
+    client.login(process.env.BOT_TOKEN);
